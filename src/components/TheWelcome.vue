c98ec76bfc11b31092ffe5415eeca402ffe9c9ab
Iurii Kondrakov <55366304+Deezzir@users.noreply.github.com>
2022-07-15 19:15:08 -0400

    tailwind configured & init stylings

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,17 +1,10 @@
 <script setup lang="ts">
 import WelcomeItem from "./WelcomeItem.vue";
-import DocumentationIcon from "./icons/IconDocumentation.vue";
 import ToolingIcon from "./icons/IconTooling.vue";
-import EcosystemIcon from "./icons/IconEcosystem.vue";
-import CommunityIcon from "./icons/IconCommunity.vue";
-import SupportIcon from "./icons/IconSupport.vue";
 </script>
 
 <template>
     <WelcomeItem>
-        <template #icon>
-            <DocumentationIcon />
-        </template>
         <template #heading>Documentation</template>
 
         Vue’s
@@ -40,9 +33,6 @@ import SupportIcon from "./icons/IconSupport.vue";
     </WelcomeItem>
 
     <WelcomeItem>
-        <template #icon>
-            <EcosystemIcon />
-        </template>
         <template #heading>Ecosystem</template>
 
         Get official tools and libraries for your project:
@@ -57,9 +47,6 @@ import SupportIcon from "./icons/IconSupport.vue";
     </WelcomeItem>
 
     <WelcomeItem>
-        <template #icon>
-            <CommunityIcon />
-        </template>
         <template #heading>Community</template>
 
         Got stuck? Ask your question on
@@ -74,9 +61,6 @@ import SupportIcon from "./icons/IconSupport.vue";
     </WelcomeItem>
 
     <WelcomeItem>
-        <template #icon>
-            <SupportIcon />
-        </template>
         <template #heading>Support Vue</template>
 
         As an independent project, Vue relies on community backing for its
